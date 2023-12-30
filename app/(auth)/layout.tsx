6fd356ce72088e2d89310c6b89f5a5a14ceb9308
Ajay Ravi <ajayravi2208@gmail.com>
2023-12-30 16:32:45 +0530

    auth bg & removed jobs in nav

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center">
-      {children}
+    <main className="background-div flex min-h-screen w-full items-center justify-end max-sm:justify-center">
+      <div className="right-10 max-h-screen overflow-y-auto md:absolute md:p-2 md:pr-10">
+        {children}
+      </div>
     </main>
   );
 }
